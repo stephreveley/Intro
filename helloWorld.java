91f123038fc8bc6078baf6fde6b7d5df07b4de33
Steph <stephreveley@rhtech.co.uk>
2016-01-26 11:23:52 +0000

    Welsh translation

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World".
         System.out.println("Hello, World");
+        System,out.println("Croeso, Byd")
     }
 
 }
